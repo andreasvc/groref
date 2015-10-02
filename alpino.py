ab3bf2666ecf66e034cd414be8f748a9cdf7ae31
Rob van der Goot <robvanderg@live.nl>
2015-10-02 16:55:54 +0200

    Started on Alpino script, works for one file only now

---
+++ alpino.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+def parse(xmlFile, dep):
+    tmpFile = open('tmp', 'w')
+    for line in open(xmlFile, 'r'):
+        if len(line) > 1:
+            tmpFile.write(line.split('\t')[6] + ' ')
+        else:
+            tmpFile.write('\n\n')
+    tmpFile.close()
+    if dep:
+        cmd = os.popen('cat tmp | Alpino number_analyses=1 end_hook=triples -parse > tmp2')
+    else:
+        cmd = os.popen('cat tmp | Alpino number_analyses=1 end_hook=syntax -parse > tmp2')
+
+def write(xmlFile, dep):
+    if dep:
+        parseFile = open(xmlFile + '.dep', 'w')
+    else:
+        parseFile = open(xmlFile + '.syn', 'w')
+    sentIdx = 1
+    for line in open('tmp2', 'r'):
+        if int(line.split('|')[3]) == sentIdx:
+            parseFile.write(line)
+        else:
+            sentIdx += 1
+            parseFile.write('\n')
+    parseFile.close()
+
+def runAlpino(working):
+    tmpPath = 'tmp'
+    for xmlFile in os.listdir(working):
+        parse(working+xmlFile, True)
+        write(working+xmlFile)
+        parse(working+xmlFile, False)
+        write(working+xmlFile)
+if __name__ == '__main__':
+    runAlpino('DCOI/conll/')
+
