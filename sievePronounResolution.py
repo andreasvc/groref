7347d37576f633bcf874b871e94ca7fe523a4e4f
Hessel Haagsma <hessel.haagsma@rug.nl>
2015-11-02 13:27:06 +0100

    Implemented first version of pronoun resolution, without matching of unknown attributes

--- sievePronounResolution.py
+++ sievePronounResolution.py
@@ -22,6 +22,9 @@ def sievePronounResolution(mention_id_list, mention_dict, cluster_dict, cluster_
 		'''Check for constraints on the anaphor here'''
 		if anaphor.type.lower() != 'pronoun': # Skip non-pronouns
 			continue
+#		else:
+#			print 'Checking this anaphor:'
+#			print anaphor.tokenList, anaphor.type, anaphor.number, anaphor.gender, anaphor.animacy, anaphor.person
 		for sent_id in range(anaphor.sentNum, max(0, anaphor.sentNum-3), -1): # Cycle through sentences backwards, but through mentions within a sentence forwards
 			if madeLink:
 				break
@@ -32,14 +35,20 @@ def sievePronounResolution(mention_id_list, mention_dict, cluster_dict, cluster_
 					if candidate_mention_id == anaphor.ID: # Don't look ahead of anaphor
 						break
 					candidate_cluster = cluster_dict[mention_dict[candidate_mention_id].clusterID]
+#					print 'looking at cluster %d' % candidate_cluster.ID
 					'''Check things against the candidate mention here'''
 					for ment_id in candidate_cluster.mentionList:
 						'''Check things against other mentions in the candidate cluster here, if necessary'''
+						matchNumber = False
+						matchGender = False
+						matchPerson = False
+						matchNER = False
 						cluster_mention = mention_dict[ment_id]
+#						print cluster_mention.tokenList, cluster_mention.type, cluster_mention.number, cluster_mention.gender, cluster_mention.animacy, cluster_mention.NEtype
 						if cluster_mention.number == anaphor.number:
 							matchNumber = True
 						if cluster_mention.gender == anaphor.gender:
-							matchNumber = True
+							matchGender = True
 						if anaphor.animacy == 'animate':
 							if cluster_mention.NEtype == 'person' or cluster_mention.NEtype == 'misc':
 								matchNER = True
@@ -54,5 +63,7 @@ def sievePronounResolution(mention_id_list, mention_dict, cluster_dict, cluster_
 						if madeLink:
 							if verbosity == 'high':
 								print 'Linking clusters %d and %d' % (ment_id, anaphor.ID)
+								cluster_dict, cluster_id_list = mergeClustersByMentionIDs(candidate_mention_id, anaphor.ID, \
+									mention_dict, cluster_dict, cluster_id_list)
 							break
 	return mention_id_list, mention_dict, cluster_dict, cluster_id_list
