a0674726c3e3309c14f17f478eca6883065f9407
DiekeO <dieke.m.oele@gmail.com>
2015-11-04 16:53:09 +0100

    predicate nominative in precise constructs

--- sievePreciseConstructs.py
+++ sievePreciseConstructs.py
@@ -1,58 +1,7 @@
 #!/usr/bin/env python
 
 from utils import *
-from demonyms import Demo
-
-def Acronyms(tokenlist):
-	ac = []
-	for token in tokenlist:
-		ac.append(token[0])
-	return ''.join(ac)
-	
-def get_rel_pron(root, pron):
-	relative_pron = []
-	for node in root.iter('node'):
-		at = node.attrib
-		if 'cat' in at and at['cat'] == 'np':
-			for child in node:
-				if 'rel' in child.attrib and child.attrib['rel'] == 'mod':
-					for c in child:
-						ca = c.attrib
-						if 'vwtype' in ca and ca['vwtype'] == 'betr' and ca['id'] == pron['id']:
-							for child in node:
-								if 'rel' in child.attrib and child.attrib['rel'] == 'hd':
-									rel_pron = (child.attrib, ca)
-									relative_pron.append(rel_pron)
-	return relative_pron
-
-def appositive(root):
-	ment = []
-	app = []
-	cnj = False
-	for node in root.iter('node'):
-		at = node.attrib
-		if 'cat' in at and at['cat'] == 'np' and at['rel'] != 'app':
-			for child in node:
-				if 'rel' in child.attrib and child.attrib['rel'] == 'app' and 'root' not in child.attrib and 'mwu_root' not in child.attrib:
-					for h in node:
-						if 'root' in h.attrib:
-							ment.append(h.attrib['id'])
-						if 'mwu_root' in h.attrib:
-							ment.append(h.attrib['id'])
-							for node in h:
-								if 'rel' in node.attrib and node.attrib['rel'] == 'mwp':									
-									ment.append(node.attrib['id'])
-					for c in child.iter('node'):
-						if 'root' in c.attrib:
-							app.append(c.attrib['id'])
-						if 'mwu_root' in c.attrib:
-							app.append(c.attrib['id'])
-							
-						if 'rel' in c.attrib and at['rel'] == 'cnj':
-							cnj = True
-	if not cnj:
-		return ment, app	
-		
+from demonyms import Demo	
 			
 '''Precise Constructs'''
 def sievePreciseConstructs(mention_id_list, mention_dict, cluster_dict, cluster_id_list, verbosity):
@@ -78,25 +27,33 @@ def sievePreciseConstructs(mention_id_list, mention_dict, cluster_dict, cluster_
 					for ment_id in candidate_cluster.mentionList:
 						root = mention_dict[ment_id].tree.getroot()
 						#print mention_dict[ment_id].tokenAttribs, mention_dict[anaphor.ID].headWords, mention_dict[anaphor.ID].tokenAttribs	
-						
 						'''Appositive'''
 						#link two mentions if they are in an appositive contstructions
-						#third children of a parent NP whose expansion begins with (NP, NP) when there is not a conjunction in the expansion	
 						appos = appositive(root)
 						if appos:
 							for ids in mention_dict[ment_id].tokenAttribs:
-								for ana_ids in mention_dict[ment_id].tokenAttribs:
-									if ids['id'] in appos[0] and ana_ids['id'] in appos[1]:
-											madeLink = True
-											print mention_dict[ment_id].tokenList, mention_dict[anaphor.ID].tokenList, appos						
-						
+								for ana_ids in mention_dict[anaphor.ID].tokenAttribs:
+									if (ids['id'], ids['root'])  in appos[0] and (ana_ids['id'], ana_ids['root']) in appos[1]:
+											madeLink = True					
 						'''Predicate nominative'''
 						#Two mentions (nominal or pronominal) are in a copulative subject-object relation
-	
-						
+						pred = pred_nom(root)
+						if pred:
+							for ids in mention_dict[ment_id].tokenAttribs:
+								for ana_ids in mention_dict[anaphor.ID].tokenAttribs:
+									if (ids['id'], ids['root'])  in pred[0] and (ana_ids['id'], ana_ids['root']) in pred[1]:
+											madeLink = True		
 						'''Role appositive'''
-				
-						
+						if mention_dict[anaphor.ID].NEtype == 'person': #and  mention_dict[ment_id].animacy == 'animate':# and mention_dict[anaphor.ID].gender != 'neuter':
+							hw = mention_dict[ment_id].headWords + mention_dict[anaphor.ID].tokenList
+							h =  mention_dict[ment_id].headWords	
+							t = []			
+							for hw in mention_dict[anaphor.ID].tokenList:
+								h.append(hw.lower())
+							for tk in mention_dict[ment_id].tokenList:
+								t.append(tk.lower())
+							if h == t:
+								madeLink == True
 						'''Relative Pronoun'''					
 						#mention is a relative pronoun that modifies the head of the antecedent NP
 						if  mention_dict[anaphor.ID].pron_type == 'betr':
@@ -133,4 +90,85 @@ def sievePreciseConstructs(mention_id_list, mention_dict, cluster_dict, cluster_
 							break
 	return mention_id_list, mention_dict, cluster_dict, cluster_id_list
 
+def Acronyms(tokenlist):
+	ac = []
+	for token in tokenlist:
+		ac.append(token[0])
+	return ''.join(ac)
+	
+def get_rel_pron(root, pron):
+	relative_pron = []
+	for node in root.iter('node'):
+		at = node.attrib
+		if 'cat' in at and at['cat'] == 'np':
+			for child in node:
+				if 'rel' in child.attrib and child.attrib['rel'] == 'mod':
+					for c in child:
+						ca = c.attrib
+						if 'vwtype' in ca and ca['vwtype'] == 'betr' and ca['id'] == pron['id']:
+							for child in node:
+								if 'rel' in child.attrib and child.attrib['rel'] == 'hd':
+									rel_pron = (child.attrib, ca)
+									relative_pron.append(rel_pron)
+	return relative_pron
 
+def appositive(root):
+	ment = []
+	app = []
+	cnj = False
+	for node in root.iter('node'):
+		at = node.attrib
+		if 'cat' in at and at['cat'] == 'np' and at['rel'] != 'app':
+			for child in node:
+				if 'rel' in child.attrib and child.attrib['rel'] == 'app' and 'root' not in child.attrib and 'mwu_root' not in child.attrib:
+					for h in node:
+						if 'root' in h.attrib:
+							ment.append((h.attrib['id'], h.attrib['root']))
+						if 'mwu_root' in h.attrib:
+							h.attrib['root'] = h.attrib['mwu_root']
+							ment.append((h.attrib['id'], h.attrib['root']))
+							for node in h:
+								if 'rel' in node.attrib and node.attrib['rel'] == 'mwp':									
+									ment.append((node.attrib['id'], node.attrib['root']))
+					for c in child.iter('node'):
+						if 'root' in c.attrib:
+							app.append((c.attrib['id'], c.attrib['root']))
+						if 'mwu_root' in c.attrib:
+							c.attrib['root'] = c.attrib['mwu_root']
+							app.append((c.attrib['id'], c.attrib['root']))
+							
+						if 'rel' in c.attrib and at['rel'] == 'cnj':
+							cnj = True
+	if not cnj:
+		return ment, app	
+		
+def pred_nom(root):
+	copula = False		
+	subj = [] 	
+	predc = []
+	for node in root.findall(".//node[@cat='smain']"):
+		for n in node:
+			at = n.attrib
+			if 'sc' in at and at['sc'] == 'copula':
+				copula = True
+		if copula:
+			for n in node:
+				at = n.attrib
+				if 'rel' in at and at['rel'] == 'su':
+					for s in n:
+						at = s.attrib
+						if 'root' in at:
+							subj.append((at['id'], at['root'])) 						
+						if 'mwu_root' in at:
+							s.attrib['root'] = s.attrib['mwu_root']
+							subj.append((at['id'], at['root']))
+				if 'rel' in at and at['rel'] == 'predc':
+					for s in n:
+						at = s.attrib
+						if 'root' in at:
+							predc.append((at['id'], at['root']))						
+						if 'mwu_root' in at:
+							s.attrib['root'] = s.attrib['mwu_root']
+							predc.append((at['id'], at['root']))
+	if len(subj) > 0 and len(predc) > 0:
+		return (subj, predc) 
