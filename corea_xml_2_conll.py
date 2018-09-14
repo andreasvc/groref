a1696bf6520418db94544504e79e134f25ca9f19
Andreas van Cranenburgh <andreas@unstable.nl>
2018-09-14 11:55:32 +0200

    formatting, remove globals

--- corea_xml_2_conll.py
+++ corea_xml_2_conll.py
@@ -1,32 +1,38 @@
 # -*- coding: utf8 -*-
-"""
-Script for converting the COREA data in xml format to the conll-format output, preserving all annotation, etc.
-Conll-format: http://conll.cemantix.org/2011/data.html
-"""
+"""Script for converting the COREA data in xml format to the conll-format
+output, preserving all annotation, etc.
+Conll-format: http://conll.cemantix.org/2011/data.html"""
 
-import os, sys, re, argparse, csv
+import os
+import re
+import sys
+import csv
+import argparse
 import xml.etree.ElementTree as ET
 
-# Adds token and appropriate id's to tokenArray
-def addTokens(text, id_list):
+
+# COREA has 4 types of links: bound, bridge, ident(ity) and pred(icate)
+# Types to include in output:
+coref_types = ['ident', 'pred']
+
+
+def addTokens(text, id_list, tokenArray):
+    """Adds token and appropriate IDs to tokenArray"""
     for token in text.split(' '):
         if token:
             tokenArray.append([token] + id_list)
 
 
-# Function that recursively goes through markables (mentions)
-def processMarkable(parent_markable, id_list):
-    if (
-        'id' in parent_markable.attrib
-    ):  # Check whether actual markable, or sentence
-        mention_id = re.findall('[0-9]+', parent_markable.attrib['id'])[
-            0
-        ]  # Extract mention id
+def processMarkable(parent_markable, id_list, tokenArray, coreferential_ids):
+    """Function that recursively goes through markables (mentions)"""
+    # Check whether actual markable, or sentence
+    if 'id' in parent_markable.attrib:
+        # Extract mention id
+        mention_id = re.findall('[0-9]+', parent_markable.attrib['id'])[0]
         if 'type' in parent_markable.attrib:  # Check whether it is coreferent
-            if (
-                parent_markable.attrib['type'] in coref_types
-                and 'ref' in parent_markable.attrib
-            ):  # Check for right type of coreference
+            # Check for right type of coreference
+            if (parent_markable.attrib['type'] in coref_types
+                and 'ref' in parent_markable.attrib):
                 ref = parent_markable.attrib['ref']
                 if ref != 'empty':
                     ref_id = re.findall('[0-9]+', ref)[0]
@@ -34,45 +40,43 @@ def processMarkable(parent_markable, id_list):
                     mention_id = ref_id
         id_list.append(mention_id)
     if parent_markable.text:
-        addTokens(parent_markable.text, id_list)
-    if (
-        parent_markable.find('markable') is not None
-    ):  # if it contains other markables
+        addTokens(parent_markable.text, id_list, tokenArray)
+    # if it contains other markables
+    if parent_markable.find('markable') is not None:
         for child_markable in parent_markable.findall('markable'):
-            processMarkable(child_markable, id_list)
+            processMarkable(
+                    child_markable, id_list, tokenArray, coreferential_ids)
     if id_list:
         id_list.pop()
     if parent_markable.tail:
-        addTokens(parent_markable.tail, id_list)
+        addTokens(parent_markable.tail, id_list, tokenArray)
 
 
-# Add round brackets indicating mention begin and end
 def postProcessTokenArray(tokenArray, coreferential_ids, no_inner=False):
+    """Add round brackets indicating mention begin and end"""
     seenList = []
     for idx, token in enumerate(tokenArray):
         for idx2, mention_id in enumerate(token):
             if idx2 > 0:  # Skip first entry, which is the token itself
                 original_mention_ids = []
-                if (
-                    mention_id in coreferential_ids
-                ):  # Resolve coreferential links, set all mentions that corefer to have same id, that of first mention of the coreference cluster
+                # Resolve coreferential links, set all mentions that corefer to
+                # have same id, that of first mention of the coreference
+                # cluster
+                if mention_id in coreferential_ids:
                     while mention_id in coreferential_ids:
-                        original_mention_ids.append(
-                            mention_id
-                        )  # Keep this to check for closing bracket
+                        # Keep this to check for closing bracket
+                        original_mention_ids.append(mention_id)
                         mention_id = coreferential_ids[mention_id]
                     token[idx2] = mention_id
-                if (
-                    mention_id not in seenList
-                ):  # Check whether needs opening bracket
+                # Check whether needs opening bracket
+                if mention_id not in seenList:
                     token[idx2] = '(' + mention_id
                     seenList.append(mention_id)
-                if (
-                    idx != len(tokenArray) - 1
-                ):  # Check whether needs closing bracket
-                    if mention_id not in tokenArray[idx + 1][1:] and not set(
-                        original_mention_ids
-                    ).intersection(set(tokenArray[idx + 1][1:])):
+                # Check whether needs closing bracket
+                if idx != len(tokenArray) - 1:
+                    if (mention_id not in tokenArray[idx + 1][1:]
+                            and not set(original_mention_ids).intersection(
+                                set(tokenArray[idx + 1][1:]))):
                         token[idx2] += ')'
                         seenList.remove(mention_id)
                 else:
@@ -84,8 +88,11 @@ def postProcessTokenArray(tokenArray, coreferential_ids, no_inner=False):
     return tokenArray
 
 
-# Function to remove mentions that have exact same span. Sometimes these are annotated that way in COREA and this causes trouble with the scorer. No nice way to do automatically,  so do it with ugly per-document per-mention rules
 def removeDuplicates(tokenArray, filename):
+    """Function to remove mentions that have exact same span. Sometimes these
+    are annotated that way in COREA and this causes trouble with the scorer. No
+    nice way to do automatically,  so do it with ugly per-document per-mention
+    rules"""
     if filename == 'WR-P-P-H-0000000077_inline.xml':
         for token in tokenArray:
             if token[0] != '134' and '134' in token[1:]:
@@ -93,80 +100,68 @@ def removeDuplicates(tokenArray, filename):
     return tokenArray
 
 
-if __name__ == '__main__':
-    # COREA has 4 types of links: bound, bridge, ident(ity) and pred(icate)
-    # Types to include in output:
-    coref_types = ['ident', 'pred']
-
+def main():
     # Parse input argument
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'input_dir',
-        type=str,
-        help='Path to a directory containing XML-files with COREA corpus data',
-    )
+            'input_dir',
+            type=str,
+            help='Path to a directory containing XML-files '
+                'with COREA corpus data')
     parser.add_argument(
-        '--noInner',
-        help='If this flag is given, corefIDs are only printed for first and last word of mention',
-        dest='no_inner',
-        action='store_true',
-    )
+            '--noInner',
+            help='If this flag is given, corefIDs are only printed for first '
+                'and last word of mention',
+            dest='no_inner',
+            action='store_true')
     parser.add_argument(
-        '--docTags',
-        help='If this flag is given, a begin and end document is printed at first and last line of output',
-        dest='doc_tags',
-        action='store_true',
-    )
+            '--docTags',
+            help='If this flag is given, a begin and end document is printed '
+                'at first and last line of output',
+            dest='doc_tags',
+            action='store_true')
     parser.add_argument(
-        '--printHeader',
-        help='If this flag is provided, a header is printed with column names',
-        dest='print_header',
-        action='store_true',
-    )
+            '--printHeader',
+            help='If this flag is provided, a header is printed '
+                'with column names',
+            dest='print_header',
+            action='store_true')
     args = parser.parse_args()
 
     # Create output dir
     if not os.path.exists(args.input_dir + '/conll'):
         os.mkdir(args.input_dir + '/conll')
 
-        # Process files
+    # Process files
     filenames = os.listdir(args.input_dir)
-    header = [
-        'doc_id',
-        'paragraph_id',
-        'paragraph_sentence_id',
-        'doc_sentence_id',
-        'sentence_token_id',
-        'doc_token_id',
-        'token',
-        'coref_id',
-    ]
+    header = ['doc_id', 'paragraph_id', 'paragraph_sentence_id',
+            'doc_sentence_id', 'sentence_token_id', 'doc_token_id', 'token',
+            'coref_id']
     for filename in filenames:
         if re.search('_inline.xml$', filename):
             sys.stdout.write('Processing file: %s\n' % (filename))
             # Parse the xml file
             try:  # Empty file in there causes trouble
                 tree = ET.parse(args.input_dir + '/' + filename)
-            except:
-                print('Processing error')
+            except Exception as err:
+                print('Processing error', filename, err)
                 continue
             root = tree.getroot()
             # Create output file and writer
             conll_file = open(
-                args.input_dir + '/conll/' + filename + '.conll', 'w'
-            )
+                    args.input_dir + '/conll/' + filename + '.conll', 'w')
             conll_writer = csv.writer(
-                conll_file, delimiter='\t', quoting=csv.QUOTE_NONE, quotechar=''
-            )
+                    conll_file, delimiter='\t',
+                    quoting=csv.QUOTE_NONE, quotechar='')
             if args.print_header:
                 conll_writer.writerow(header)
             docName = filename.split('_')[0]
             if args.doc_tags:
                 conll_writer.writerow(
-                    ['#begin document (' + docName + '); part 000']
-                )
+                        ['#begin document (' + docName + '); part 000'])
 
-            coreferential_ids = {}  # dict containing mappings of ids which corefer
+            # dict containing mappings of ids which corefer
+            coreferential_ids = {}
             doc_token_id = 0
             doc_sentence_id = 0
             for sentence in root.iter('sentence'):
@@ -175,50 +170,44 @@ if __name__ == '__main__':
                 doc_sentence_id += 1
                 try:
                     alpsent = sentence.attrib['alpsent']
-                    paragraph_id = re.findall('p.[0-9]+', alpsent)[0][
-                        2:
-                    ]  # Extract paragraph number
-                    sentence_id = re.findall('s.[0-9]+', alpsent)[0][
-                        2:
-                    ]  # Extract sentence number
+                    # Extract paragraph number
+                    paragraph_id = re.findall('p.[0-9]+', alpsent)[0][2:]
+                    # Extract sentence number
+                    sentence_id = re.findall('s.[0-9]+', alpsent)[0][2:]
                 except IndexError:
                     paragraph_id = '-1'
                     sentence_id = '-1'
-                processMarkable(sentence, [])  # Actual processing
+                # Actual processing
+                processMarkable(sentence, [], tokenArray, coreferential_ids)
                 tokenArray.pop()  # remove trailing newline
                 tokenArray = removeDuplicates(tokenArray, filename)
                 tokenArray = postProcessTokenArray(
-                    tokenArray, coreferential_ids
-                )
+                        tokenArray, coreferential_ids)
                 for token in tokenArray:
                     sentence_token_id += 1
                     doc_token_id += 1
-                    if (
-                        args.no_inner
-                    ):  # Do not print cluster IDs except for at start and end of mention
+                    # Do not print cluster IDs except for at start and end of
+                    # mention
+                    if args.no_inner:
                         new_IDs = []
                         for old_ID in token[1].split('|'):
-                            if (
-                                '(' in old_ID or ')' in old_ID
-                            ):  # Check for brackets
+                            # Check for brackets
+                            if '(' in old_ID or ')' in old_ID:
                                 new_IDs.append(old_ID)
                         token[1] = '|'.join(new_IDs)
                         if not new_IDs:  # If empty
                             token[1] = '-'
                     conll_writer.writerow(
-                        [
-                            docName,
-                            paragraph_id,
-                            sentence_id,
-                            doc_sentence_id,
-                            sentence_token_id,
-                            doc_token_id,
-                            token[0],
-                        ]
-                        + token[1:]
-                    )
+                            [docName, paragraph_id, sentence_id,
+                                doc_sentence_id, sentence_token_id,
+                                doc_token_id, token[0]]
+                            + token[1:])
                 conll_writer.writerow([])
             if args.doc_tags:
                 conll_writer.writerow(['#end document'])
             conll_file.close()
     print('Done!')
+
+
+if __name__ == '__main__':
+    main()
--- sievePronounResolution.py
+++ sievePronounResolution.py
@@ -1,103 +1,97 @@
-'''Pronoun resolution sieve'''
+"""Pronoun resolution sieve"""
 
 from utils import mergeClustersByMentionIDs, get_mention_id_list_per_sentence
 
 
-def sievePronounResolution(
-    mention_id_list, mention_dict, cluster_dict, cluster_id_list, verbosity
-):
+def sievePronounResolution(mention_id_list, mention_dict, cluster_dict,
+        cluster_id_list, verbosity):
     if verbosity == 'high':
         print('Doing pronoun resolution...')
     mention_ids_per_sentence = get_mention_id_list_per_sentence(
-        mention_id_list, mention_dict
-    )
+        mention_id_list, mention_dict)
     for cluster_id in cluster_id_list[:]:
-        '''Initialize linking constraints here'''
+        # Initialize linking constraints here
         matchNumber = False
         matchGender = False
         matchPerson = False
-        # 		matchAnimacy = False
+        # matchAnimacy = False
         matchNER = False
         madeLink = False
         if madeLink:
             continue
         cluster = cluster_dict[cluster_id]
-        anaphor = mention_dict[
-            cluster.mentionList[0]
-        ]  # Only consider first mention in cluster for resolution
-        '''Check for constraints on the anaphor here'''
+        # Only consider first mention in cluster for resolution
+        anaphor = mention_dict[cluster.mentionList[0]]
+        # Check for constraints on the anaphor here
         if anaphor.type.lower() != 'pronoun':  # Skip non-pronouns
             continue
-        # 		else:
-        # 			print 'Checking this anaphor:'
-        # 			print anaphor.tokenList, anaphor.type, anaphor.number, anaphor.gender, anaphor.animacy, anaphor.person
-        for sent_id in range(
-            anaphor.sentNum, max(0, anaphor.sentNum - 3), -1
-        ):  # Cycle through sentences backwards, but through mentions within a sentence forwards
+        if verbosity == 'high':
+            print('Checking this anaphor:',
+                    anaphor.tokenList, {
+                    'type': anaphor.type,
+                    'numb': anaphor.number,
+                    'gend': anaphor.gender,
+                    'anim': anaphor.animacy,
+                    'pers': anaphor.person})
+
+        # Cycle through sentences backwards, but through mentions within a
+        # sentence forwards
+        for sent_id in range(anaphor.sentNum, max(0, anaphor.sentNum - 3), -1):
             if madeLink:
                 break
             if sent_id in mention_ids_per_sentence:  # Not empty
                 for candidate_mention_id in mention_ids_per_sentence[sent_id]:
                     if madeLink:
                         break
-                    if (
-                        candidate_mention_id == anaphor.ID
-                    ):  # Don't look ahead of anaphor
+                    # Don't look ahead of anaphor
+                    if candidate_mention_id == anaphor.ID:
                         break
                     candidate_cluster = cluster_dict[
-                        mention_dict[candidate_mention_id].clusterID
-                    ]
-                    # 					print 'looking at cluster %d' % candidate_cluster.ID
-                    '''Check things against the candidate mention here'''
+                            mention_dict[candidate_mention_id].clusterID]
+                    if verbosity == 'high':
+                        print('looking at cluster %d' % candidate_cluster.ID)
+                    # Check things against the candidate mention here
                     for ment_id in candidate_cluster.mentionList:
-                        '''Check things against other mentions in the candidate cluster here, if necessary'''
+                        # Check things against other mentions in the candidate
+                        # cluster here, if necessary
                         matchNumber = False
                         matchGender = False
                         matchPerson = False
                         matchNER = False
                         cluster_mention = mention_dict[ment_id]
-                        # 						print cluster_mention.tokenList, cluster_mention.type, cluster_mention.number, cluster_mention.gender, cluster_mention.animacy, cluster_mention.NEtype
-                        if (
-                            cluster_mention.number == anaphor.number
-                            or anaphor.number == 'unknown'
-                        ):
+                        if verbosity == 'high':
+                            print(cluster_mention.tokenList,
+                                    {
+                                        'type': cluster_mention.type,
+                                        'numb': cluster_mention.number,
+                                        'gend': cluster_mention.gender,
+                                        'anim': cluster_mention.animacy,
+                                        'netype': cluster_mention.NEtype})
+                        if (cluster_mention.number == anaphor.number
+                                or anaphor.number == 'unknown'):
                             matchNumber = True
-                        if (
-                            cluster_mention.gender == anaphor.gender
-                            or anaphor.gender == 'unknown'
-                        ):
+                        if (cluster_mention.gender == anaphor.gender
+                                or anaphor.gender == 'unknown'):
                             matchGender = True
                         if anaphor.animacy == 'animate':
-                            if (
-                                cluster_mention.NEtype == 'person'
-                                or cluster_mention.NEtype == 'misc'
-                            ):
+                            if (cluster_mention.NEtype == 'person'
+                                    or cluster_mention.NEtype == 'misc'):
                                 matchNER = True
-                            if (
-                                cluster_mention.NEtype == ''
-                                or cluster_mention.NEtype == 'unknown'
-                            ):
+                            if (cluster_mention.NEtype == ''
+                                    or cluster_mention.NEtype == 'unknown'):
                                 matchNER = True
                         else:
                             if cluster_mention.NEtype != 'person':
                                 matchNER = True
-                            if (
-                                cluster_mention.NEtype == ''
-                                or cluster_mention.NEtype == 'unknown'
-                            ):
+                            if (cluster_mention.NEtype == ''
+                                    or cluster_mention.NEtype == 'unknown'):
                                 matchNER = True
                         if cluster_mention.type.lower() == 'pronoun':
-                            if (
-                                cluster_mention.person == anaphor.person
-                                or anaphor.person == 'unknown'
-                            ):
+                            if (cluster_mention.person == anaphor.person
+                                    or anaphor.person == 'unknown'):
                                 matchPerson = True
-                            if (
-                                matchNumber
-                                and matchGender
-                                and matchPerson
-                                and matchNER
-                            ):
+                            if (matchNumber and matchGender
+                                    and matchPerson and matchNER):
                                 madeLink = True
                         else:
                             if matchNumber and matchGender and matchNER:
@@ -105,15 +99,11 @@ def sievePronounResolution(
                         if madeLink:
                             if verbosity == 'high':
                                 print('Linking clusters %d and %d' % (
-                                    ment_id,
-                                    anaphor.ID,
-                                ))
-                                cluster_dict, cluster_id_list = mergeClustersByMentionIDs(
-                                    candidate_mention_id,
-                                    anaphor.ID,
-                                    mention_dict,
-                                    cluster_dict,
-                                    cluster_id_list,
-                                )
+                                        ment_id, anaphor.ID))
+                            (cluster_dict, cluster_id_list
+                                    ) = mergeClustersByMentionIDs(
+                                    candidate_mention_id, anaphor.ID,
+                                    mention_dict, cluster_dict,
+                                    cluster_id_list)
                             break
     return mention_id_list, mention_dict, cluster_dict, cluster_id_list
